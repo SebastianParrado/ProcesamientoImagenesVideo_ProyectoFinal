99463e2518178624bb65ecb4b1ec76772c60f32a
Andrea Juliana Ruiz Gomez <andreajulianaruiz@hotmail.com>
2020-12-02 20:21:53 -0500

    Update interfaz_class.py
    grabcut and gui

--- interfaz_class.py
+++ interfaz_class.py
@@ -10,6 +10,7 @@ from tkinter.filedialog import askopenfilename
 from PIL import Image # Para mostrar la imagen en la interfaz GUI
 from PIL import ImageTk # Para mostrar la imagen en la interfaz GUI
 import cv2
+import numpy as np
 #import sys
 
 ###Step 1: Create The App Frame
@@ -53,9 +54,19 @@ class AppFrame(tk.Frame):
         self.btn_w = 110 # self.tamanio horizontal boton
         self.btn_h = 30 # self.tamanio vertical boton
         
+        self.gui()
+        
+        self.select_reset()
+        
+    def gui(self):
         """
         Textos en la ventana
         """
+        lbli = tk.Label(self, text=" ", font=("Arial Bold", 40)) # Configuracion boton (ventana, texto, estilo y self.tamaño de letra)
+        lbli.place(x=620, y=self.btn_posy, width=self.tam, height=self.tam) # Posicion y self.tamanio texto
+        lbli.configure(bg=self.wn_bg,fg=self.wn_fg) # Colores texto
+        #lbli.bind('<Motion>',self.motion)
+        
         lbl0 = tk.Label(self, text=" ", font=("Arial Bold", 40)) # Configuracion boton (ventana, texto, estilo y self.tamaño de letra)
         lbl0.place(x=0, y=0, width=1370, height=820) # Posicion y self.tamanio texto
         lbl0.configure(bg=self.wn_bg,fg=self.wn_fg) # Colores texto
@@ -104,14 +115,63 @@ class AppFrame(tk.Frame):
         # btn7.place(x=self.btn_posx, y=(self.btn_posy2+(n*40)), width=self.btn_w, height=self.btn_h) # Posicion y self.tamanio boton 7
         
         n = 1  # Hace referencia a la operacion de la posicion en y del boton 8
-        btn8 = tk.Button(self, text="Exit", bg=self.but_bg, fg=self.but_fg, command=self.select_quit) # Configuracion del boton 8 (ventana, texto, colores de fondo y de letra, funcion)
+        btn8 = tk.Button(self, text="Reset", bg=self.but_bg, fg=self.but_fg, command=self.select_reset) # Configuracion del boton 8 (ventana, texto, colores de fondo y de letra, funcion)
         btn8.place(x=self.btn_posx, y=(self.btn_posy2+(n*40)), width=self.btn_w, height=self.btn_h) # Posicion y self.tamanio boton 8
 
         
         ###Create button
         #btn = tk.Button(self, text='askopenfilename',command=self.askopenfilename)
         #btn.pack(pady=5)
-
+    def mouse_move(self,event):
+        x = event.x#-10
+        y = event.y#-self.btn_posy
+        if self.flag_rect == True:
+            img_temp_m = self.image_in.copy()
+            self.fin_points = [x , y]
+            self.img_copy = cv2.rectangle(img_temp_m, tuple(self.ini_points), tuple(self.fin_points), (0, 0, 255), 5)    
+            print("inipoitns",self.ini_points)
+        if self.flag_circle_fg == True and self.start == True: 
+            print("Circulo fg")
+            cv2.circle(self.img_copy, (x, y), 3, (255,255,255), -1)
+            cv2.circle(self.mask, (x, y), 5, 1, -1)    
+        elif self.flag_circle_bg == True and self.start == True: 
+            print("Circulo bg")
+            cv2.circle(self.img_copy, (x, y), 3, (0,0,0), -1)
+            cv2.circle(self.mask, (x, y), 5, 0, -1)
+        self.trans_show_images(self.img_copy,self.image_out)   
+        
+        #print("Mouse position: (%s %s)" % (event.x, event.y))
+        
+    def mouse_down(self,event):
+        x = event.x#-10
+        y = event.y#-self.btn_posy
+        #if x>0 and y>0:
+        print("Click en: (%s %s)" % (event.x, event.y))
+        if self.flag_rect == True:
+            self.ini_points = [x , y]
+        #else:
+         #   print("Fuera de imagen")
+        if ((self.flag_rect == False) and ((self.flag_circle_fg == True) or (self.flag_circle_bg == True))):
+            self.start = True
+            print("Circulo start")
+        
+    def mouse_up(self,event):
+        print("Suelto en: (%s %s)" % (event.x, event.y))
+        x = event.x#-10
+        y = event.y#-self.btn_posy
+        if self.flag_rect == True:
+            img_temp = self.image_in.copy()
+            self.fin_points = [x , y]
+            self.img_copy = cv2.rectangle(img_temp, tuple(self.ini_points), tuple(self.fin_points), (0, 0, 255), 5)
+            self.mask = cv2.rectangle(self.mask, tuple(self.ini_points), tuple(self.fin_points), 3, -1)
+            self.corners = self.ini_points[0],self.ini_points[1],self.fin_points[0],self.fin_points[1]
+            #self.flag_rect = False
+            print('Rectangulo terminado')
+            #cv2.destroyWindow('image_mod')
+            self.flag_rect = False
+        self.start = False
+        self.trans_show_images(self.img_copy,self.image_out)     
+        
     def askopenfilename(self):
         ###ask filepath
         filepath = askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("png files","*.png"),("all files","*.*"))) # Abre el buscador de archivos
@@ -127,69 +187,90 @@ class AppFrame(tk.Frame):
         print("ENTRO A SELECCIONAR IMAGEN")
         self.askopenfilename()
         self.image_in = cv2.imread(self.filepath) # load the image from disk
-        self.image_out = cv2.imread(self.filepath) # load the image from disk
         self.image_in = cv2.resize(self.image_in,(self.tam,self.tam))
+        self.img_copy = self.image_in.copy()
+        self.img_bg = np.zeros((self.tam,self.tam,3),np.uint8)
+        self.image_out = cv2.imread(self.filepath) # load the image from disk
         self.image_out = cv2.resize(self.image_out,(self.tam,self.tam))
-        self.trans_show_images()
+        self.trans_show_images(self.image_in,self.image_out)
+        
+        self.gui()
         # self.image_in = self.askopenfilename
         # self.image_out = self.askopenfilename
     # Funcion para transformar y mostrar imagenes en GUI      
-    def trans_show_images(self):
+    def trans_show_images(self,image_in,image_out):
         # OpenCV represents images in BGR order; however PIL represents
   		# images in RGB order, so we need to swap the channels
-        self.image_in = cv2.cvtColor(self.image_in, cv2.COLOR_BGR2RGB)
-        self.image_out = cv2.cvtColor(self.image_out, cv2.COLOR_BGR2RGB)
+        image_in = cv2.cvtColor(image_in, cv2.COLOR_BGR2RGB)
+        image_out = cv2.cvtColor(image_out, cv2.COLOR_BGR2RGB)
   		# convert the images to PIL format...
-        self.image_in = Image.fromarray(self.image_in)
-        self.image_out = Image.fromarray(self.image_out)
+        image_in = Image.fromarray(image_in)
+        image_out = Image.fromarray(image_out)
   		# ...and then to ImageTk format
-        self.image_in = ImageTk.PhotoImage(self.image_in)
-        self.image_out = ImageTk.PhotoImage(self.image_out)
+        image_in = ImageTk.PhotoImage(image_in)
+        image_out = ImageTk.PhotoImage(image_out)
           
           # if the panels are None, initialize them
         if self.panelA is None or self.panelB is None:
             # the first panel will store our original imag
-            self.panelA = tk.Label(image=self.image_in)
-            self.panelA.image = self.image_in
+            self.panelA = tk.Label(image=image_in)
+            self.panelA.image = image_in
             self.panelA.place(x=10, y=self.btn_posy, width=self.tam, height=self.tam)
             # while the second panel will store the edge map
-            self.panelB = tk.Label(image=self.image_out)
-            self.panelB.image = self.image_out       
+            self.panelB = tk.Label(image=image_out)
+            self.panelB.image = image_out       
             self.panelB.place(x=620, y=self.btn_posy, width=self.tam, height=self.tam)
             # otherwise, update the image panels
         else:
             # update the pannels
-            self.panelA.configure(image=self.image_in)
-            self.panelB.configure(image=self.image_out)
-            self.panelA.image = self.image_in
-            self.panelB.image = self.image_out
+            self.panelA.configure(image=image_in)
+            self.panelB.configure(image=image_out)
+            self.panelA.image = image_in
+            self.panelB.image = image_out
+        
+        self.panelA.bind('<Motion>',self.mouse_move)        
+        self.panelA.bind('<Button-1>', self.mouse_down)
+        self.panelA.bind('<ButtonRelease-1>', self.mouse_up)
     
                 
     # Funcion de hacer el rectangulo
     def select_rectangle(self):
         print("HOLA")  
+        self.flag_rect = True
+        self.flag_circle_fg = False
+        self.flag_circle_bg = False
     
     # Funcion para seleccionar fondo
     def select_background(self):
+        self.flag_rect = False
+        self.flag_circle_fg = False
+        self.flag_circle_bg = True
         print("HOLA BG")  
     
     # Funcion para seleccionar primer plano
     def select_foreground(self):
+        self.flag_rect = False
+        self.flag_circle_fg = True
+        self.flag_circle_bg = False
         print("HOLA FG")
     
     # Funcion para realizar iteracion
     def select_iteration(self):
         print("HOLA IT")
+        self.flag_rect = False
+        self.flag_circle_fg = False
+        self.flag_circle_bg = False
+        self.iteration()
     
     # Funcion para seleccionar imagenes del fondo
     def select_bg(self):
         print("HOLA FG sel")
         self.askopenfilename()
-        self.image_in = cv2.imread(self.filepath) # load the image from disk
-        self.image_out = cv2.imread(self.filepath) # load the image from disk
-        self.image_in = cv2.resize(self.image_in,(self.tam,self.tam))
-        self.image_out = cv2.resize(self.image_out,(self.tam,self.tam))
-        self.trans_show_images()
+        self.img_bg = cv2.imread(self.filepath) # load the image from disk
+        #self.image_out = cv2.imread(self.filepath) # load the image from disk
+        self.img_bg = cv2.resize(self.img_bg,(self.tam,self.tam))
+        #self.image_out = cv2.resize(self.image_out,(self.tam,self.tam))
+        #self.trans_show_images(self.image_in,self.image_out)
         
     # Funcion para seleccionar o quitar pantalla completa
     def select_fs(self):
@@ -199,52 +280,42 @@ class AppFrame(tk.Frame):
         else: # Si no esta en pantalla completa...
             self.state = True # ... ponga pantalla completa
         self.attributes("-fullscreen", self.state) # Ajustar el estado de la pantalla
-        
+    
+    def iteration(self):
+        cv2.grabCut(self.image_in, self.mask, None, self.BGD_model, self.FGD_model, 1, cv2.GC_INIT_WITH_MASK)
+        self.mask_out = np.where((self.mask==1)|(self.mask==3), 1, 0).astype('uint8')
+        output = cv2.bitwise_and(self.image_in, self.image_in, mask=self.mask_out)
+        # #cv2.imshow('image_mod',output)
+        self.mask_bg = np.where((self.mask==1)|(self.mask==3), 0, 1).astype('uint8')
+        # self.img_bg = cv2.imread('lena.jpg', 1)  
+        # self.img_bg = cv2.resize(self.img_bg,(self.tam,self.tam))
+        output_bg = cv2.bitwise_or(self.img_bg,self.img_bg, mask=self.mask_bg)
+        self.image_out = cv2.bitwise_or(output_bg,output)
+        # cv2.imshow('image_mod',output_bg)
+        self.trans_show_images(self.img_copy,self.image_out)
+         
     # Funcion para cerrar la ventana
-    def select_quit(self):
-        #global window  # variables globales usadas
-        self.destroy()
-        #sys.exit()
-
+    def select_reset(self):
+        self.BGD_model = np.zeros((1,65),np.float64)
+        self.FGD_model = np.zeros((1,65),np.float64)
+        self.ini_points , self.fin_points , self.temp_points  , self.corners = [],[],[],[]
+        self.flag_rect = False #Rect = True
+        self.flag_circle_fg = False
+        self.flag_circle_bg = False
+        self.start = False
+        self.flag_rect_or_mask = True #Rect_iteration = True / mask_iteration = False
+        self.initial_mask = np.zeros((self.tam,self.tam),np.uint8)
+        self.mask = np.zeros((self.tam,self.tam),np.uint8)
+        self.img_bg = np.zeros((self.tam,self.tam,3),np.uint8)
+        self.trans_show_images(self.img_bg,self.img_bg)
 
 ###Step 2: Creating The App
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         ###call the parent constructor
         tk.Tk.__init__(self, *args, **kwargs)
-        
-         # Colores de la interfaz - Códigos de colores: https://htmlcolorcodes.com/es/
-        """
-        Morado
-        
-        self.but_bg = '#E188FD' # Fondo boton
-        self.but_fg = '#55086D' # Letra boton
-        self.wn_bg = '#F4E0FA'  # Fondo ventana
-        self.wn_fg = '#55086D'  # Letra ventana
-        """
-        """
-        Azul
-        """
-        self.but_bg = '#78CCEB' # Fondo boton
-        self.but_fg = '#0B5E7D' # Letra boton
-        self.wn_bg = '#E0F3FA'  # Fondo ventana
-        self.wn_fg = '#0B5E7D'  # Letra ventana
-        
-        # self.tamanio imagenes que se muestran en la interfaz. Las imagenes son cuadradas
-        self.tam = 600 
-        
-        # Posiciones y self.tamanios de los botones
-        self.btn_posx = 1240 # Posicion x del boton
-        self.btn_posy = 130 # Posicion y del primer bloque de botones
-        self.btn_posy2 = 660 # Posicion y del segundo bloque de botones
-        self.btn_w = 110 # self.tamanio horizontal boton
-        self.btn_h = 30 # self.tamanio vertical boton
-       
             
         self.attributes("-fullscreen", True) # Pantalla completa
-        wn_bg = '#E0F3FA'  # Fondo ventana
-        #tk.Tk(['bg'])=wn_bg
-        self.configure(bg='#E0F3FA') # Color ventana
 
         ###create filepath list
         self.filepaths = []
